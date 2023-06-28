96ce00722c9e9d61cd7f795a65c0fe5f26470a29
A.Polyakov <a.polyakov@unlimint.com>
2023-06-28 21:03:22 +0300

    add test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
+    Calcalator calcalator = new Calcalator();
+
+    @Test
+    public void testSum() {
+        int a = 2;
+        int b = 5;
+        Assert.assertEquals(7, calcalator.sum(a, b));
+    }
+
+    @Test
+    public void testSub() {
+        int a = 2;
+        int b = 5;
+        Assert.assertEquals(-3, calcalator.sub(a, b));
+    }
 }
